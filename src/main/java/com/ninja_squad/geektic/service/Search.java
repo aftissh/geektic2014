a8495d927408c8d6a8158c900c333bd835e9e197
unknown <p0905121@IUTDOUAB2PC14.univ-lyon1.fr>
2015-06-22 16:16:23 +0200

    update Multiple Views, Routing and Layout Template

---
+++ src/main/java/com/ninja_squad/geektic/service/Search.java
@@ -0,0 +1,10 @@
+package com.ninja_squad.geektic.service;
+
+/**
+ * Created by p0905121 on 22/06/2015.
+ */
+public class Search {
+    public String getURL() {
+        return "/geek-list.html";
+    }
+}
--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -6,28 +6,29 @@ app.controller('HelloCtrl', function($scope, $http) {
     });
 });
 
-app.controller('Recherche', function($scope, $http) {
-    $http.get('/geek-list.html').success(function(helloMessage) {
-    $scope.helloMessage = helloMessage;
+app.controller('RechercheCtrl', function($scope, $location) {
+    $scope.recherche = function(){
+        $location.path('/list');
+    }
 });
 
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: '/main.html',
-            controller: 'Recherche'
+            templateUrl: '../view/main.html',
+            controller: 'RechercheCtrl'
         })
         .when('/list', {
-            templateUrl: '/geek-list.html',
+            templateUrl: '../view/geek-list.html',
             controller: 'GeekListCtrl'
         })
         .when('/details', {
-            templateUrl: '/geek-details.html',
+            templateUrl: '../view/geek-details.html',
             controller: 'GeekDetailsCtrl'
         })
         .otherwise({
-            redirectTo: '/main.html'
+            redirectTo: '/'
         });
 }]);
 
